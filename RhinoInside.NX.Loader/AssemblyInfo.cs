a92ab4d9c855426bc554de75cbbee49c88722848
Changsheng <ZCSTKHK@163.com>
2021-06-03 14:11:24 +0800

    0.0.3 Add a C# Script Component for RhinoInside NX

--- RhinoInside.NX.Loader/AssemblyInfo.cs
+++ RhinoInside.NX.Loader/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("0.0.3.0")]
+[assembly: AssemblyFileVersion("0.0.3.0")]
